9e74db44a1ea3bca2442906b29cd32ce0e572ff4
Chris Agostino <cjp.agostino@gmail.com>
2025-09-07 16:02:00 -0400

    testing buildyaml

--- npc_studio_serve.py
+++ npc_studio_serve.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     
     start_flask_server(
         port="5337",   
-        cors_origins="localhost:5173", 
+        cors_origins="localhost:6337", 
         db_path = os.path.expanduser('~/npcsh_history.db'),
         user_npc_directory = os.path.expanduser('~/.npcsh/npc_team'), 
         debug=False)
